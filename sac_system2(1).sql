fbf6e1092013770ea6fb0bc4cf5e18090a1bd0a4
Dominic Nzube Simeon <simeondominic@gmail.com>
2025-06-15 07:07:02 -0600

    update to sac_system2.sql

--- sac_system2(1).sql
+++ sac_system2(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 09, 2025 at 01:15 AM
+-- Generation Time: Jun 15, 2025 at 03:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -76,18 +76,35 @@ CREATE TABLE `users` (
   `sac` varchar(20) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT current_timestamp(),
-  `fullname` varchar(100) DEFAULT NULL
+  `fullname` varchar(100) DEFAULT NULL,
+  `surname` varchar(100) NOT NULL,
+  `middle_name` varchar(100) DEFAULT NULL,
+  `other_name` varchar(100) DEFAULT NULL,
+  `gender` varchar(10) NOT NULL,
+  `dob` date DEFAULT NULL,
+  `state` varchar(100) NOT NULL,
+  `lga` varchar(100) NOT NULL,
+  `school` varchar(150) DEFAULT NULL,
+  `department` varchar(150) DEFAULT NULL,
+  `course_level` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `marital_status` varchar(20) DEFAULT NULL,
+  `user_type` enum('student','staff') NOT NULL,
+  `id_number` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `sac`, `password`, `created_at`, `fullname`) VALUES
-(1, 'simeondominic9@gmail.com', 'QWE456', '', '2025-06-08 02:24:09', 'Simeon Nzube Dominic'),
-(4, 'simeonadrian22@gmail.com', 'AXE789', '', '2025-06-08 02:30:24', 'Simeon Nzube'),
-(5, 'simeonadrian504@gmail.com', '68472636052', '', '2025-06-08 15:54:10', 'Shimasu Philip'),
-(7, 'simeonchiemerie@gmail.com', '58923052651', '', '2025-06-08 17:04:41', 'Simeon Chiemerie');
+INSERT INTO `users` (`id`, `email`, `sac`, `password`, `created_at`, `fullname`, `surname`, `middle_name`, `other_name`, `gender`, `dob`, `state`, `lga`, `school`, `department`, `course_level`, `phone`, `address`, `marital_status`, `user_type`, `id_number`) VALUES
+(1, 'simeondominic9@gmail.com', 'QWE456', '', '2025-06-08 02:24:09', 'Simeon Nzube Dominic', '', NULL, NULL, '', NULL, '', '', NULL, NULL, NULL, '', '', NULL, 'student', ''),
+(4, 'simeonadrian22@gmail.com', 'AXE789', '', '2025-06-08 02:30:24', 'Simeon Nzube', '', NULL, NULL, '', NULL, '', '', NULL, NULL, NULL, '', '', NULL, 'student', ''),
+(5, 'simeonadrian504@gmail.com', '68472636052', '', '2025-06-08 15:54:10', 'Shimasu Philip', '', NULL, NULL, '', NULL, '', '', NULL, NULL, NULL, '', '', NULL, 'student', ''),
+(7, 'simeonchiemerie@gmail.com', '58923052651', '', '2025-06-08 17:04:41', 'Simeon Chiemerie', '', NULL, NULL, '', NULL, '', '', NULL, NULL, NULL, '', '', NULL, 'student', ''),
+(8, 'emmanuelnysc@gmail.com', '18258371779', '', '2025-06-10 05:52:23', 'Emmanuel', '', NULL, NULL, '', NULL, '', '', NULL, NULL, NULL, '', '', NULL, 'student', ''),
+(9, 'simeonagina@gmail.com', '36127646025', '', '2025-06-15 06:59:53', NULL, 'Simeon', 'Agina', 'Godwin', 'Male', '1998-10-01', 'Enugu', 'Aniniri', 'Engineering', 'Civil Engineering', 'Final Year', '08143045241', 'Auchi Edo State', 'Single', 'student', '2017030883');
 
 --
 -- Indexes for dumped tables
@@ -149,7 +166,7 @@ ALTER TABLE `sac_codes`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
